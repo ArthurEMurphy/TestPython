72f7d7dbe9137e8ba4d0f7315d78adf851dad0bf
ArthurEMurphy <101611099+ArthurEMurphy@users.noreply.github.com>
2022-05-28 14:57:50 -0400

    End of session 3
    FSDI 108

--- blueberry_backend/server.py
+++ blueberry_backend/server.py
@@ -1,7 +1,7 @@
 import json
 from flask import Flask
 from about_me import me
-from mock_data import catalog #IMPORTANT STEP
+from mock_data import catalog  # IMPORTANT STEP
 
 app = Flask('blueberry')
 
@@ -25,11 +25,34 @@ def address():
 
 # JSON - JaveScript Object Notation
 ############################################################################################# API ENDPOINTS ##################################################################################################
+# Postman -> Test endpoints of REST APIs
 
 @app.route("/api/catalog", methods=["GET"])
 def get_catalog():
     return json.dumps(catalog)
 
+
+@app.route("/api/catalog/count", methods=["GET"])
+def get_count():
+    # Here... count how many products are in the list catalog
+    counts = len(catalog)
+
+    return json.dumps(counts)  # return the value
+
+# Request 127.0.0.1:5000/api/product/5f40a6baac77a903d8f682c6
+
+
+@app.route("/api/product/<id>", methods=["GET"])
+def get_product(id):
+    # cheapest =
+
+    return json.dumps(id)
+
+# @app.route("/api/total/<id>", methods=["GET"])
+# def get_total():
+    # total =
+
+    # return json.dumps(total)
 
 
 app.run(debug=True)
