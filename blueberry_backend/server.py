6a8d7892556df8dbd6cedf258330eb11b568e5fc
ArthurEMurphy <101611099+ArthurEMurphy@users.noreply.github.com>
2022-06-01 22:53:12 -0400

    End of session 4
    FSDI 108

--- blueberry_backend/server.py
+++ blueberry_backend/server.py
@@ -1,5 +1,7 @@
+from email.mime import application
 import json
-from flask import Flask
+from pickle import APPEND
+from flask import Flask, abort  # Django
 from about_me import me
 from mock_data import catalog  # IMPORTANT STEP
 
@@ -10,7 +12,7 @@ app = Flask('blueberry')
 def home():
     return "This is the home page"
 
-# Create an about endpoint and show
+# Create an about endpoint and show your name
 
 
 @app.route("/about")
@@ -23,13 +25,15 @@ def address():
     return f'  {me["address"]["number"]} {me["address"]["street"]}'
 
 
-# JSON - JaveScript Object Notation
+# JSON - JaveScript Object Notation API - Application Program Interface
 ############################################################################################# API ENDPOINTS ##################################################################################################
 # Postman -> Test endpoints of REST APIs
 
 @app.route("/api/catalog", methods=["GET"])
 def get_catalog():
     return json.dumps(catalog)
+
+# make an endpoint to send back how mant product do we have in the catalog
 
 
 @app.route("/api/catalog/count", methods=["GET"])
@@ -44,15 +48,86 @@ def get_count():
 
 @app.route("/api/product/<id>", methods=["GET"])
 def get_product(id):
-    # cheapest =
+    # find the product whose _id is equal to id
+    for prod in catalog:
+        if prod["_id"] == id:
+            return json.dumps(prod)
+    # catalog
+    # travel calatlog with for loop
+    # get every product inside the list
+    # if the _id of the prodcut is equal to the id variable
+    # found it, return that product as JSON
 
-    return json.dumps(id)
+    # return an error code
+    return abort(404, "Id does not match any product")
 
-# @app.route("/api/total/<id>", methods=["GET"])
-# def get_total():
-    # total =
 
-    # return json.dumps(total)
+# Create an endpoint that returns the SUM of all the products' price
+# GET /api/catalog/total
+# @app.route("/api/catalog/total", methods=["GET"])
+@app.get('/api/catalog/total')
+def get_total():
+    total = 0
+    for prod in catalog:
+        #total = total + prod["price"]
+        total += prod["price"]
+    return json.dumps(total)
+
+
+# get product by category
+# get /api/products/<category>
+
+@app.get('/api/products/<category>')
+def products_by_category(category):
+    results = []
+    category = category.lower()
+    for prod in catalog:
+        if prod['category'].lower() == category:
+            results.append(prod)
+
+    return json.dumps(results)
+
+# get the list of categories
+# get /api/categories
+
+
+@app.get('/api/categories')
+def get_unique_categories():
+    results = []
+    for prod in catalog:
+        cat = prod['category']
+        # if cat does not exist in results, then
+        if not cat in results:
+            results.append(cat)
+    return json.dumps(results)
+
+
+# get the cheapest product
+@app.get('/api/products/cheapest')
+def get_cheapest_product():
+    solution = catalog[0]
+    for prod in catalog:
+        if prod['price'] < solution['price']:
+            solution = prod
+    return json.dumps(solution)
+
+
+@app.get("/api/exercise1")
+def get_exe1():
+    nums = [123, 123, 654, 124, 8865, 532, 4768, 8476, 45762,
+            345, -1, 234, 0, -12, -456, -123, -865, 532, 4768]
+    solution = {}
+
+    # A: print the lowest number
+    solution['a'] = 1
+    # B: count and print how many numbers are lower than 500
+    solution['b'] = 1
+    # sum all the negatives
+    solution['c'] = 1
+    # find the sum of numbers except negatives
+    solution['d'] = 1
+
+    return json.dumps(solution)
 
 
 app.run(debug=True)
